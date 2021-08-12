9c41ceb1cbc204dfeeced2a76c13bcaa3939fb48
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-08-12 15:32:11 +0200

    add doubleClick handler on folder

--- src/components/folder/folder.component.tsx
+++ src/components/folder/folder.component.tsx
@@ -1,30 +1,52 @@
-import classes from './folder.module.css';
+import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import useDoubleClick from 'use-double-click';
 import { selectFiles } from '../../store/store';
 import { updateCurrentView, updateHistoryView } from '../../store/filesSlice';
 import FolderIcon from '../../images/folderIcon.png';
 
+import classes from './folder.module.css';
+
 const Folder = ({ folderName, files, folders }): JSX.Element => {
+  const [activeFolder, setActiveFolder] = useState(false);
   const dispatch = useDispatch();
   const filesState = useSelector(selectFiles);
-
   const currentView = {
     folderName: folderName,
     files: files,
     folders: folders,
   };
-  const updateViewHandler = () => {
-    dispatch(updateHistoryView(filesState.currentView));
-    dispatch(updateCurrentView(currentView));
-  };
-  return (
-    <div className={classes.folder} onClick={updateViewHandler}>
-      <div className={classes.folder__iconWrapper}>
-        <img src={FolderIcon} className={classes.folder__icon} alt="folderIcon" />
+
+  const SingleFolder = () => {
+    const buttonRef = useRef();
+
+    useDoubleClick({
+      onSingleClick: () => {
+        setActiveFolder(true);
+        setTimeout(function () {
+          setActiveFolder(false);
+        }, 1000);
+      },
+      onDoubleClick: () => {
+        setActiveFolder(false);
+        dispatch(updateHistoryView(filesState.currentView));
+        dispatch(updateCurrentView(currentView));
+      },
+      ref: buttonRef,
+      latency: 250,
+    });
+
+    return (
+      <div className={classes.folder} ref={buttonRef}>
+        <div className={activeFolder ? classes.folder__iconWrapperActive : classes.folder__iconWrapper}>
+          <img src={FolderIcon} className={classes.folder__icon} alt="folderIcon" />
+        </div>
+        <p className={activeFolder ? classes.folder__nameActive : classes.folder__name}>{folderName}</p>
       </div>
-      <p className={classes.folder__name}>{folderName}</p>
-    </div>
-  );
+    );
+  };
+
+  return <SingleFolder />;
 };
 
 export default Folder;
